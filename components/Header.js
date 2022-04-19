11c41a2f733570d663ece70eb6d0e0d8b9b5752d
ctdeakin <ctdeakin@gmail.com>
2022-04-19 16:09:49 -0400

    removed unused directory

--- components/Header.js
+++ components/Header.js
@@ -3,6 +3,6 @@
 export default function Header({ title }) {
   return <h1>
     {title}
-  <style jsx>{`h1 { color: red;}`}</style>
+  <style jsx>{`h1 { color: red; position: center;}`}</style>
   </h1>
 }
